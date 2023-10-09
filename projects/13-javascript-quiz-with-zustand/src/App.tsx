59a6b532e506a2d3087423723c47402229097982
Frainer Encarnación <fraineralex2001@gmail.com>
2023-10-09 14:03:58 -0400

    add: add the javascript quiz with zustand project

---
+++ projects/13-javascript-quiz-with-zustand/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App () {
+  return (
+    <>
+      <h1>JavaScript Quiz</h1>
+    </>
+  )
+}
+
+export default App
---
+++ projects/13-javascript-quiz-with-zustand/src/main.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <App />
+)
