ff1cc52b57382cd4b695965ea783f7a609eabff5
Frainer Encarnación <fraineralex2001@gmail.com>
2023-10-20 15:40:21 -0400

    fix: use the custom hook useFilters in the App.tsx

--- projects/11-typescript-technical-test/src/App.tsx
+++ projects/11-typescript-technical-test/src/App.tsx
@@ -3,68 +3,20 @@ import { SortBy, User } from './types.d'
 import './App.css'
 import { UserList } from './components/UserList'
 import { useGetAllUsers } from './hooks/useGetAllUsers'
+import { useFilters } from './hooks/useFilters'
 
 function App () {
-  const { users, setUsers, originalUsers } = useGetAllUsers()
-  const [showColor, setShowColor] = useState(false)
-  const [sorting, setSorting] = useState<SortBy>(SortBy.NONE)
-  const [filterCountry, setFilterCountry] = useState<string | null>(null)
-
-  const toggleColors = () => {
-    setShowColor(!showColor)
-  }
-
-  const toggleSortByCountry = () => {
-    const newSortingValue =
-      sorting === SortBy.COUNTRY ? SortBy.NONE : SortBy.COUNTRY
-    setSorting(newSortingValue)
-  }
-
-  const handleReset = () => {
-    setUsers(originalUsers.current)
-    setFilterCountry(null)
-    const input = document.querySelector('input')
-    setShowColor(false)
-    if (input) {
-      input.value = ''
-    }
-  }
-
-  const handleDeleteUser = (email: string) => {
-    const filteredUsers = users.filter(user => user.email !== email)
-    setUsers(filteredUsers)
-  }
-
-  const handleChangeSort = (sortingValue: SortBy) => {
-    const newSortingValue =
-      sorting === sortingValue ? SortBy.NONE : sortingValue
-    setSorting(newSortingValue)
-  }
-
-  const filteredUsers = useMemo(() => {
-    return filterCountry !== null && filterCountry.length > 0
-      ? users.filter(user => {
-          return user.location.country
-            .toLowerCase()
-            .includes(filterCountry.toLowerCase())
-        })
-      : users
-  }, [filterCountry, users])
-
-  const sortedUsers = useMemo(() => {
-    if (sorting === SortBy.NONE) return filteredUsers
-
-    const compareProperties: Record<string, (user: User) => any> = {
-      [SortBy.COUNTRY]: user => user.location.country,
-      [SortBy.NAME]: user => user.name.first,
-      [SortBy.LAST]: user => user.name.last
-    }
-
-    return filteredUsers.toSorted((a,b) => {
-      const extractProperty = compareProperties[sorting]
-      return extractProperty(a).localeCompare(extractProperty(b))
-    })
-  }, [filteredUsers, sorting])
+  const {
+    sorting,
+    showColor,
+    setFilterCountry,
+    sortedUsers,
+    handleChangeSort,
+    handleDeleteUser,
+    handleReset,
+    toggleSortByCountry,
+    toggleColors
+  } = useFilters()
 
   return (
     <>
