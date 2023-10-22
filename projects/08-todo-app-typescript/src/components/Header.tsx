af639faf176a6bcd9b4cc6579b70e6efe84a15e0
Frainer Encarnación <fraineralex2001@gmail.com>
2023-10-22 00:10:56 -0400

    fix: add some styles to the ui design

--- projects/08-todo-app-typescript/src/components/Header.tsx
+++ projects/08-todo-app-typescript/src/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC<Props> = ({ saveTodo }) => {
     <header className='header'>
       <h1>todo
         <img
-          style={{ width: '60px', height: 'auto' }}
+          style={{ width: '60px', height: 'auto', marginLeft: 5 }}
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png'></img>
       </h1>
 
