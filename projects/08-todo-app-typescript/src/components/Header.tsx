480d9bf6db3bc04111131483d9f13f84a66a356f
Frainer Encarnación <fraineralex2001@gmail.com>
2023-10-21 23:56:32 -0400

    fix: add the image of typescript to the header

--- projects/08-todo-app-typescript/src/components/Header.tsx
+++ projects/08-todo-app-typescript/src/components/Header.tsx
@@ -1,19 +1,16 @@
-import { TodoTitle } from "../types";
-import { CreateTodo } from "./CreateTodo";
+import { CreateTodo } from './CreateTodo'
 
 interface Props {
-    saveTodo: ({ title }: TodoTitle) => void;
+  saveTodo: (title: string) => void
 }
 
 export const Header: React.FC<Props> = ({ saveTodo }) => {
   return (
     <header className='header'>
-      <h1>
-        todo
+      <h1>todo
         <img
           style={{ width: '60px', height: 'auto' }}
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png'
-        />
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png'></img>
       </h1>
 
       <CreateTodo saveTodo={saveTodo} />
