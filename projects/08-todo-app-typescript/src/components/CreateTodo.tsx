23e7fb02f3b30f5b18ae17e285802be3960bc2dd
Frainer Encarnación <fraineralex2001@gmail.com>
2023-10-21 23:53:10 -0400

    fix: replece the controlades form by an incontrolated input with the keydown enter to create todos

--- projects/08-todo-app-typescript/src/components/CreateTodo.tsx
+++ projects/08-todo-app-typescript/src/components/CreateTodo.tsx
@@ -1,28 +1,27 @@
 import { useState } from 'react'
-import { TodoTitle } from '../types'
 
 interface Props {
-  saveTodo: ({ title }: TodoTitle) => void
+  saveTodo: (title: string) => void
 }
 
 export const CreateTodo: React.FC<Props> = ({ saveTodo }) => {
   const [inputValue, setInputValue] = useState('')
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault()
-    saveTodo({ title: inputValue })
-    setInputValue('')
+  const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Enter' && inputValue !== '') {
+      saveTodo(inputValue)
+      setInputValue('')
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        className='new-todo'
-        value={inputValue}
-        onChange={event => setInputValue(event.target.value)}
-        placeholder='What needs to be done?'
-        autoFocus
-      />
-    </form>
+    <input
+      className='new-todo'
+      value={inputValue}
+      onChange={(e) => { setInputValue(e.target.value) }}
+      onKeyDown={handleKeyDown}
+      placeholder='What needs to be done?'
+      autoFocus
+    />
   )
 }
