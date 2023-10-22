d4b6cf9cc062caf5b928ad6bcba24b71350fb968
Frainer Encarnación <fraineralex2001@gmail.com>
2023-10-22 00:03:31 -0400

    add: add a custom footer with the informations of the project and a link to my github

---
+++ projects/08-todo-app-typescript/src/components/Copyright.tsx
@@ -0,0 +1,12 @@
+import './Copyright.css'
+
+export function Copyright () {
+  return (
+    <footer className='copyright' style={{ textAlign: 'center', color: 'black' }}>
+      <strong>
+        TODO App with animations － <a target='a_blanc' href='https://github.com/fraineralex'>@fraineralex</a>
+      </strong>
+      <p>Build with React, TypeScript and animations.</p>
+    </footer>
+  )
+}
