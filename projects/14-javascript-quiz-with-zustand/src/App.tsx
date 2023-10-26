17326df2071a7621c187438ab212454bf6306ea8
Frainer Encarnación <fraineralex2001@gmail.com>
2023-10-25 21:19:01 -0400

    fix: add the require changes to show the python quiz option

--- projects/14-javascript-quiz-with-zustand/src/App.tsx
+++ projects/14-javascript-quiz-with-zustand/src/App.tsx
@@ -1,34 +1,42 @@
 import './App.css'
 import { Container, Stack, Typography } from '@mui/material'
-import { JavaScriptLogo } from './JavaScriptLogo'
-import { Start } from './Start'
+import { LanguageLogo } from './components/LanguagesLogos'
+import { Start } from './components/Start'
 import { useQuestionsStore } from './store/questions'
-import { Game } from './Game'
+import { Game } from './components/Game'
+import { Header } from './components/Header'
+import { Contact } from './components/Contact'
 
 function App () {
   const questions = useQuestionsStore(state => state.questions)
-
-  console.log(questions)
+  const quiz = useQuestionsStore(state => state.quiz)
 
   return (
-    <main>
-      <Container maxWidth='sm'>
-        <Stack
-          direction='row'
-          gap={2}
-          alignItems='center'
-          justifyContent='center'
-        >
-          <JavaScriptLogo />
-          <Typography variant='h2' component='h1'>
-            <strong>JavaScript Quiz</strong>
-          </Typography>
-        </Stack>
+    <>
+      <Header />
+      <main>
+        <Container maxWidth='sm'>
+          <Stack
+            direction='row'
+            gap={2}
+            alignItems='center'
+            justifyContent='center'
+          >
+            <LanguageLogo language={quiz} />
+            <Typography variant='h2' component='h1'>
+              <strong>
+                {quiz === 'js' && 'JavaScript Quiz'}
+                {quiz === 'py' && 'Python Quiz'}
+              </strong>
+            </Typography>
+          </Stack>
 
-        {questions.length === 0 && <Start />}
-        {questions.length > 0 && <Game />}
-      </Container>
-    </main>
+          {questions.length === 0 && <Start />}
+          {questions.length > 0 && <Game />}
+        </Container>
+      </main>
+      <Contact />
+    </>
   )
 }
 
--- projects/14-javascript-quiz-with-zustand/src/types.d.ts
+++ projects/14-javascript-quiz-with-zustand/src/types.d.ts
@@ -1,3 +1,4 @@
+import { type OverridableStringUnion } from '@material-ui/types'
 export interface Question {
   id: number
   question: string
@@ -6,4 +7,30 @@ export interface Question {
   correctAnswer: number
   userSelectedAnswer?: number
   isCorrectUserAnswer?: boolean
+}
+
+export type quizzes = 'js' | 'py'
+
+interface SoporteQuizzesType {
+  Object: {
+    language: quizzes
+    text: string
+    color: string
+    backgroundColor: string
+  }
+}
+
+export const SoporteQuizzes: SoporteQuizzesType = {
+  js: {
+    language: 'js' as quizzes,
+    text: 'JavaScript Quiz',
+    color: 'black',
+    backgroundColor: 'white',
+  },
+  py: {
+    language: 'py' as quizzes,
+    text: 'Python Quiz',
+    color: 'black',
+    backgroundColor: 'white'
+  }
 }
